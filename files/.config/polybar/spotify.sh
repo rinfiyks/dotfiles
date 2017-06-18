dd769b3bbd5ecad7aed95b083f7a3878654dafcf
rinfiyks <tom.gould.92@gmail.com>
2017-06-18 17:35:57 +0100

    Add paused icon to polybar spotify module

--- files/.config/polybar/spotify.sh
+++ files/.config/polybar/spotify.sh
@@ -3,8 +3,16 @@
 playerctl -p spotify metadata > /dev/null 2>&1
 
 if [[ $? == 0 ]]; then
+
+    icon=$(playerctl -p spotify status |
+        grep -q Paused
+        if [[ $? == 0 ]]; then
+            echo -e "\uf04c" # font-awesome paused icon
+            else echo -e "\uf1bc" # font-awesome spotify icon
+        fi)
+
     artist=$(playerctl -p spotify metadata xesam:artist)
     title=$(playerctl -p spotify metadata xesam:title)
 
-    echo -e "\uf1bc  $artist - $title"
+    echo -e "$icon  $artist - $title"
 fi
