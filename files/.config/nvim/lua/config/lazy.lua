b6904a19da46d48297ba9dc383c09f00a0f2a546
rinfiyks <tom.gould.92@gmail.com>
2024-04-02 22:16:32 +0100

    Neovim background changing function

--- files/.config/nvim/lua/config/lazy.lua
+++ files/.config/nvim/lua/config/lazy.lua
@@ -6,6 +6,25 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
+local function check_and_update_background_periodically()
+  local background_file = "/home/tom/.config/nvim/lua/config/background.vim"
+  local last_modified = vim.fn.getftime(background_file)
+
+  -- Assuming you use a global variable to store the last known modification time
+  if vim.g.last_background_mod and last_modified ~= vim.g.last_background_mod and last_modified > 0 then
+    vim.cmd("source " .. background_file)
+    vim.g.last_background_mod = last_modified
+  end
+end
+
+-- Set a global variable with the initial modification time
+vim.g.last_background_mod = vim.fn.getftime("/home/tom/.config/nvim/lua/config/background.vim")
+
+-- Set up a timer to check the background file every minute
+vim.defer_fn(function()
+  vim.fn.timer_start(1000, check_and_update_background_periodically, { ["repeat"] = -1 })
+end, 0)
+
 require("lazy").setup({
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
