cd2799140f8675970dda5c2cfab8dbc56cec58e1
rinfiyks <tom.gould.92@gmail.com>
2024-06-18 18:32:16 +0100

    Not using nvim-plug

--- install.sh
+++ install.sh
@@ -33,13 +33,4 @@ fi
 filesdir="$(pwd)/files"
 cp -TdRbs "$filesdir" "$HOME"
 
-if [ ! -d "$HOME/.local/share/nvim/plugged" ]; then
-  echo "Installing vim-plug for vim"
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  nvim +PlugInstall +qall
-else
-  nvim +PlugUpgrade +qall
-fi
-
 echo "All done"
