5e2630f0b1ca302f354a21f0ca5e02ab3cfceeda
rinfiyks <tom.gould.92@gmail.com>
2025-04-13 18:58:23 +0100

    Improve nvim light/dark mode changing with fs_event

--- nvim/.config/nvim/lua/core/background.lua
+++ nvim/.config/nvim/lua/core/background.lua
@@ -1,21 +1,16 @@
 local M = {}
 
-M.background_file = "~/.config/nvim/lua/config/background.vim"
+M.background_file = vim.fn.expand("~/.config/nvim/lua/config/background.vim")
+local fs_event = vim.loop.new_fs_event()
 
-local function check_and_update_background_periodically()
-    local last_modified = vim.fn.getftime(M.background_file)
-
-    -- Assuming you use a global variable to store the last known modification time
-    if vim.g.last_background_mod and last_modified ~= vim.g.last_background_mod and last_modified > 0 then
-        vim.cmd("source " .. M.background_file)
-        vim.g.last_background_mod = last_modified
-    end
+if fs_event then
+    fs_event:start(M.background_file, {}, function(err, filename, events)
+        if err then
+            vim.notify("fs_event error: " .. err)
+            return
+        end
+        vim.schedule(function() vim.cmd("source " .. M.background_file) end)
+    end)
 end
-
--- Set a global variable with the initial modification time
-vim.g.last_background_mod = vim.fn.getftime(M.background_file)
-
--- Set up a timer to check the background file every second
-vim.defer_fn(function() vim.fn.timer_start(1000, check_and_update_background_periodically, { ["repeat"] = -1 }) end, 0)
 
 return M
