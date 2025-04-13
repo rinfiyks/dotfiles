0530cfc06bedbfeefe3ebdd273f31bdaa5d22c47
rinfiyks <tom.gould.92@gmail.com>
2025-04-13 13:21:23 +0100

    Change colorscheme to nightfox and fix light/dark mode on startup

--- nvim/.config/nvim/lua/core/background.lua
+++ nvim/.config/nvim/lua/core/background.lua
@@ -1,16 +1,21 @@
+local M = {}
+
+M.background_file = "~/.config/nvim/lua/config/background.vim"
+
 local function check_and_update_background_periodically()
-    local background_file = "/home/tom/.config/nvim/lua/config/background.vim"
-    local last_modified = vim.fn.getftime(background_file)
+    local last_modified = vim.fn.getftime(M.background_file)
 
     -- Assuming you use a global variable to store the last known modification time
     if vim.g.last_background_mod and last_modified ~= vim.g.last_background_mod and last_modified > 0 then
-        vim.cmd("source " .. background_file)
+        vim.cmd("source " .. M.background_file)
         vim.g.last_background_mod = last_modified
     end
 end
 
 -- Set a global variable with the initial modification time
-vim.g.last_background_mod = vim.fn.getftime("/home/tom/.config/nvim/lua/config/background.vim")
+vim.g.last_background_mod = vim.fn.getftime(M.background_file)
 
 -- Set up a timer to check the background file every second
 vim.defer_fn(function() vim.fn.timer_start(1000, check_and_update_background_periodically, { ["repeat"] = -1 }) end, 0)
+
+return M
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 return {
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function() vim.cmd("colorscheme rose-pine") end,
+        "EdenEast/nightfox.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            local background = require("core.background")
+            vim.cmd("source " .. background.background_file)
+        end,
     },
 }
