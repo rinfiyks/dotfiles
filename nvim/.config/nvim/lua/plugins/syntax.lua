741e11fc0593a26f9f6c05d183595976c41697c7
rinfiyks <tom.gould.92@gmail.com>
2026-09-07 21:38:43 +0100

    Treesitter: js, jsonc, rust

--- nvim/.config/nvim/lua/plugins/syntax.lua
+++ nvim/.config/nvim/lua/plugins/syntax.lua
@@ -13,6 +13,7 @@ return {
                 "css",
                 "cpp",
                 "groovy",
+                "javascript",
                 "json",
                 "lua",
                 "luadoc",
@@ -22,6 +23,7 @@ return {
                 "python",
                 "query",
                 "regex",
+                "rust",
                 "scala",
                 "sql",
                 "toml",
@@ -61,22 +63,74 @@ return {
             })
 
             local ts_select = require("nvim-treesitter-textobjects.select")
-            vim.keymap.set({ "x", "o" }, "af", function() ts_select.select_textobject("@function.outer", "textobjects") end)
-            vim.keymap.set({ "x", "o" }, "if", function() ts_select.select_textobject("@function.inner", "textobjects") end)
-            vim.keymap.set({ "x", "o" }, "ac", function() ts_select.select_textobject("@class.outer", "textobjects") end)
-            vim.keymap.set({ "x", "o" }, "ic", function() ts_select.select_textobject("@class.inner", "textobjects") end)
-            vim.keymap.set({ "x", "o" }, "aa", function() ts_select.select_textobject("@parameter.outer", "textobjects") end)
-            vim.keymap.set({ "x", "o" }, "ia", function() ts_select.select_textobject("@parameter.inner", "textobjects") end)
+            vim.keymap.set(
+                { "x", "o" },
+                "af",
+                function() ts_select.select_textobject("@function.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "x", "o" },
+                "if",
+                function() ts_select.select_textobject("@function.inner", "textobjects") end
+            )
+            vim.keymap.set(
+                { "x", "o" },
+                "ac",
+                function() ts_select.select_textobject("@class.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "x", "o" },
+                "ic",
+                function() ts_select.select_textobject("@class.inner", "textobjects") end
+            )
+            vim.keymap.set(
+                { "x", "o" },
+                "aa",
+                function() ts_select.select_textobject("@parameter.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "x", "o" },
+                "ia",
+                function() ts_select.select_textobject("@parameter.inner", "textobjects") end
+            )
 
             local ts_move = require("nvim-treesitter-textobjects.move")
-            vim.keymap.set({ "n", "x", "o" }, "]f", function() ts_move.goto_next_start("@function.outer", "textobjects") end)
-            vim.keymap.set({ "n", "x", "o" }, "]F", function() ts_move.goto_next_end("@function.outer", "textobjects") end)
-            vim.keymap.set({ "n", "x", "o" }, "[f", function() ts_move.goto_previous_start("@function.outer", "textobjects") end)
-            vim.keymap.set({ "n", "x", "o" }, "[F", function() ts_move.goto_previous_end("@function.outer", "textobjects") end)
-            vim.keymap.set({ "n", "x", "o" }, "]c", function() ts_move.goto_next_start("@class.outer", "textobjects") end)
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "]f",
+                function() ts_move.goto_next_start("@function.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "]F",
+                function() ts_move.goto_next_end("@function.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "[f",
+                function() ts_move.goto_previous_start("@function.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "[F",
+                function() ts_move.goto_previous_end("@function.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "]c",
+                function() ts_move.goto_next_start("@class.outer", "textobjects") end
+            )
             vim.keymap.set({ "n", "x", "o" }, "]C", function() ts_move.goto_next_end("@class.outer", "textobjects") end)
-            vim.keymap.set({ "n", "x", "o" }, "[c", function() ts_move.goto_previous_start("@class.outer", "textobjects") end)
-            vim.keymap.set({ "n", "x", "o" }, "[C", function() ts_move.goto_previous_end("@class.outer", "textobjects") end)
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "[c",
+                function() ts_move.goto_previous_start("@class.outer", "textobjects") end
+            )
+            vim.keymap.set(
+                { "n", "x", "o" },
+                "[C",
+                function() ts_move.goto_previous_end("@class.outer", "textobjects") end
+            )
 
             local ts_swap = require("nvim-treesitter-textobjects.swap")
             vim.keymap.set("n", "<leader>a", function() ts_swap.swap_next("@parameter.inner") end)
